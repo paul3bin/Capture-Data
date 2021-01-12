88a3f4203530aa0c77e891b7492fb7fdebd107ba
Ebin Paul <ebinpaul@outlook.com>
2021-01-12 10:30:29 +0530

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class CaptureData(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.first_name
