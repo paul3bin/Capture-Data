d2ed32aa8c9159de1e42c6d65b1f4572ee691ffa
Ebin Paul <ebinpaul@outlook.com>
2021-01-12 10:30:45 +0530

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from . import serializers, models
 # Create your views here.
+
+class DataViewSet(viewsets.ModelViewSet):
+    queryset = models.CaptureData.objects.all()
+    serializer_class = serializers.CaptureDataSerializer
