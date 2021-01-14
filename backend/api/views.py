974d45ad28e9b7b624fefb6a1fd735630ef715a4
Ebin Paul <ebinpaul@outlook.com>
2021-01-14 19:14:49 +0530

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from . import serializers, models
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+    permission_classes = (IsAdminUser, )
+
 
 class DataViewSet(viewsets.ModelViewSet):
     queryset = models.CaptureData.objects.all()
