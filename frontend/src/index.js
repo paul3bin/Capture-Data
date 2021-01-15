9c5da6fdfda96dc5a5b600123d780ca4efd175d8
Ebin Paul <ebinpaul@outlook.com>
2021-01-15 12:45:46 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Route, BrowserRouter} from 'react-router-dom';
+import {CookiesProvider} from 'react-cookie';
+import { AdminLogin } from './components/admin_login'
+import { AdminLanding } from './components/admin_landing'
 
-ReactDOM.render(
+function Router(){
+  return(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+      <Route exact path='/' component={App}/>
+      <Route exact path='/admin-login' component={AdminLogin}/>
+      <Route exact path='/admin-landing' component={AdminLanding}/>
+    </BrowserRouter>
+  </React.StrictMode>
+  )
+}
 
+ReactDOM.render(<Router />,document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
