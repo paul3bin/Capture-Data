8d8d20db22411abe6dab9a707fca0b66783e3689
Ebin Paul <ebinpaul@outlook.com>
2021-01-15 12:45:38 +0530

    Create admin_landing.js

---
+++ frontend/src/components/admin_landing.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import { useCookies } from 'react-cookie';
+
+function AdminLanding() {
+
+    const [token, setToken, deleteToken] = useCookies(['token']);
+
+    const logoutUser = () => {
+        deleteToken(['token']);
+    }
+
+    useEffect(() => {
+        if(!token['token']) window.location.href = '/'
+      }, [token])
+
+    const downloadClicked = () =>{
+        alert('Download button is clicked!')
+    }
+
+    return (
+        <div className="App">
+        <header className="App-header">
+            <button className="btn btn-warning Download" onClick={downloadClicked}>Download Data</button><br/>
+            <button className="btn btn-danger Admin-Login" onClick={logoutUser}>Logout</button><br/>
+        </header>
+        </div>
+    );
+}
+
+export {AdminLanding};
